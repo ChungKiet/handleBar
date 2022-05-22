0a28bbb070067d41a7c1916f23fb4a2699eff939
Đau Mắt <66857266+leducminh85@users.noreply.github.com>
2022-05-22 11:12:33 +0700

    create task4

--- index.js
+++ index.js
@@ -1,52 +1,38 @@
 const express = require('express')
+const path = require('path')
 const expressHbs = require('express-handlebars');
+const { extname } = require('path');
+
 const app = express();
-const path = require('path');
 
 const hbs = expressHbs.create({
-   layoutsDir: path.join(__dirname, 'views/layouts'),
-   extname: 'hbs',
-   defaultLayout: 'layout'
-});
-
-const { categories, products } = require('./data')
-
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-
-app.use(express.static(path.join(__dirname, 'code')));
-
-app.get('/', (req, res)=> {
-   res.render('index');
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    extname: 'hbs',
+    defaultLayout: 'layout'
 })
 
-app.get('/task1', (req, res)=> {
-   res.render('task1');
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+
+app.use(express.static(path.join(__dirname, 'Handlebars-StaticFiles')))
+
+app.get('/', (req,res)=>{
+   res.render('index')
 })
 
-app.get('/task2', (req, res)=> {
-   res.render('task2');
+const {zodiacs } = require('./data')
+app.get('/task4', (req,res)=>{
+    res.locals.zodiacs = zodiacs;
+    res.render('task4')
 })
 
-app.get('/task3', (req, res)=> {
-   let category = req.query.cat || 0;
-   res.locals.categories = categories;
-   res.locals.products = products;
-   if (category){
-      res.locals.products = products.filter(item => item.category == category);
-   }
-   res.render('task3');
+app.get('/task4/:name', (req,res)=>{
+    res.locals.zodiac = zodiacs.filter(item=>item.name == req.params.name)[0]
+    res.render('task4-details')
 })
 
-app.get('/task4', (req, res)=> {
-   res.render('task4');
-})
-
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 5000)
 
 app.listen(app.get('port'), () => {
-   console.log(`server is running on the ${app.get('port')}`);
-   console.log(path.join(__dirname, 'code'),{
-      index: 'index.htm'
-   })
+    console.log(`server is running on port ${app.get('port')}`)
 })
